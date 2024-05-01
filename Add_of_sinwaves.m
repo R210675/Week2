9e28ad8be686d81f1c7d00bfcd4e7c514aad9c64
R210675 <159224053+R210675@users.noreply.github.com>
2024-05-01 15:04:29 +0530

    Add files via upload

---
+++ Add_of_sinwaves.m
@@ -0,0 +1,10 @@
+clc
+clear all
+close all
+t = 0:0.01:1
+f1 = 2
+f2 = 3
+x=sin(2 * pi * f1  * t)
+y=sin(2 * pi * f2  * t)
+z= x+y
+plot(t, z)
---
+++ mul_of_sinwaves.py
@@ -0,0 +1,13 @@
+import numpy as np 
+from matplotlib import pyplot as plt
+t= np.arange(0,1,0.01) 
+f1 = 2
+f2 = 3
+x = np.sin (2*np. pi * f1 * t )
+y = np.sin (2*np. pi * f2 * t )
+z=x*y
+plt.plot(t,z)
+plt.xlabel('time')
+plt.ylabel ('amplitude')
+plt.title ('sine wave')
+plt.show()
